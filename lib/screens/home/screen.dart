9c3b3c0af0e8f50923c987911d34d139bdc663d9
zikwall <andrey.kapitonov.96@gmail.com>
2021-12-07 23:59:12 +0300

    work with home screen

--- lib/screens/home/screen.dart
+++ lib/screens/home/screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
 // dependencies
-//
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 // application
 import 'cards.dart';
@@ -38,7 +38,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   @override
   void initState() {
     super.initState();
-    _animation = AnimationController(vsync: this, duration: const Duration(milliseconds: 1000),);
+    _animation = AnimationController(vsync: this, duration: const Duration(milliseconds: 500),);
     _pageController = PageController(initialPage: 0, viewportFraction: 0.8);
 
     _animation.forward();
@@ -112,11 +112,19 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                             child: PageView.builder(
                               controller: _pageController,
                               itemBuilder: (BuildContext context, int index) {
-                                if (index == _tasks.length) {
-                                  return buildCardNew(context);
-                                } else {
-                                  return buildCardCategory(context);
-                                }
+                                return AnimationConfiguration.staggeredList(
+                                  position: index,
+                                  duration: const Duration(milliseconds: 375),
+                                  child: SlideAnimation(
+                                    horizontalOffset: 6.0,
+                                    child: FadeInAnimation(
+                                      duration: const Duration(milliseconds: 30),
+                                      child: (index == _tasks.length)
+                                      ? buildCardNew(context)
+                                      : buildCardCategory(context),
+                                    ),
+                                  ),
+                                );
                               },
                               itemCount: _tasks.length + 1,
                             ),
